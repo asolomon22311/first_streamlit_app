5ea134aae36d3e0432abd1806e950edaee4c629a
asolomon22311 <126131625+asolomon22311@users.noreply.github.com>
2023-02-25 16:37:19 -0500

    Update streamlit_app.py
    organized code with try

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#new section header
+#new section header fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.erro("Please select a fruit to get information.")
+  else:
+    #streamlit.write('The user entered ', fruit_choice)
+    #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
+    # write your own comment -what does the next line do? variable to store the json response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do? -- json response will be in normalized datafram
+    streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? variable to store the json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do? -- json response will be in normalized datafram
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #don't run anything past this when troubleshooting
 streamlit.stop()
