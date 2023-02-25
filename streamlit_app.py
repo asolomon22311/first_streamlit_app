c41dd8410978ffc5982ed9a22a671109257c738f
asolomon22311 <126131625+asolomon22311@users.noreply.github.com>
2023-02-25 13:42:47 -0500

    Update streamlit_app.py
    fix typo varialble my_data_rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 
 streamlit.header("The fruit load list contain:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
